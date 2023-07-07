4f294307e9a87a58d38c186f722fa5e39b3ddb30
aashish2604 <aashish.260401@gmail.com>
2023-07-07 12:54:40 +0530

    feat: added status codes

---
+++ src/constants/status_constants.js
@@ -0,0 +1,15 @@
+class StatusConstants {
+    static code400 = 400;
+    static code400Message = "Bad Request";
+    static code401 = 401;
+    static code401Message = "Unauthorised";
+    static code404 = 404;
+    static code404Message = "Template not found";
+    static code500 = 500;
+    static code500Message = "Internal Server Error";
+    static code200 = 200;
+    static code200Message = "Request Successful";
+  }
+  
+  module.exports = StatusConstants;
+  
