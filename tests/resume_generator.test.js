c760aa777f5726692f5f2f6b0916f13ba21eb4f5
aashish2604 <aashish.260401@gmail.com>
2023-07-08 15:59:03 +0530

    updated resumer generator unit tests

--- tests/resume_generator.test.js
+++ tests/resume_generator.test.js
@@ -1,41 +1,41 @@
-const ResumeGenerator=require("../src/services/generate_resume/resume_generator");
-const Testcases=require('../src/constants/tescases');
+const ResumeGenerator = require("../src/services/generate_resume/resume_generator");
+const Testcases = require("../src/constants/tescases");
 const PDFServicesSdk = require("@adobe/pdfservices-node-sdk");
 
-const resumeGenerator=new ResumeGenerator();
-const ResumeGeneratorTestcase=Testcases.ResumeGeneratorTestcase;
-const testcases=new ResumeGeneratorTestcase();
+const resumeGenerator = new ResumeGenerator();
+const ResumeGeneratorTestcase = Testcases.ResumeGeneratorTestcase;
+const testcases = new ResumeGeneratorTestcase();
 
-describe('Resume Generator Tester',()=>{
+describe("Resume Generator Tester", () => {
+  test("corresponding template value are returned for their ids", () => {
+    const result1 = resumeGenerator.getTemplateDoc("1");
+    const result2 = resumeGenerator.getTemplateDoc("2");
+    const result3 = resumeGenerator.getTemplateDoc("3");
 
-    test('corresponding template value are returned for their ids',()=>{
-        const result1=resumeGenerator.getTemplateDoc("1");
-        const result2=resumeGenerator.getTemplateDoc("2");
-        const result3=resumeGenerator.getTemplateDoc("3");
+    expect(result1).toBe("BasicTemplate.docx");
+    expect(result2).toBe("ImageTemplate.docx");
+    expect(result3).toBe("LinkTemplate.docx");
+  });
 
-        expect(result1).toBe("BasicTemplate.docx");
-        expect(result2).toBe("ImageTemplate.docx");
-        expect(result3).toBe("LinkTemplate.docx");
+  test("generate function works fine for valid inputs", async () => {
+    try {
+      const result = await resumeGenerator.generate(testcases.validInput, "1");
+      expect(result).toBeInstanceOf(PDFServicesSdk.FileRef);
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  }, 15000);
+
+  test("should throw an error if an error occurs during generation", async () => {
+    const mockError = new Error("jsonDataForMerge is required.");
+
+    // Mocking the execution to throw an error
+    resumeGenerator.execute = jest.fn(() => {
+      throw mockError;
     });
 
-    test('generate function works fine for valid input',async ()=>{
-        try {
-            const result=await resumeGenerator.generate(testcases.validInput,"1");
-            expect(result).toBeInstanceOf(PDFServicesSdk.FileRef);
-        } catch (error) {
-            expect(error).toBeDefined();
-        }
-        
-    },15000);
-
-    test('generate function throws error if jsonData is not provided',async ()=>{
-        try {
-            await resumeGenerator.generate(undefined,"1");
-        } catch (error) {
-            expect(error).toBeDefined();
-        }
-        
-    });
-
-
-})
+    await expect(() =>
+      resumeGenerator.generate(undefined, "1")
+    ).rejects.toThrow(mockError);
+  });
+});
