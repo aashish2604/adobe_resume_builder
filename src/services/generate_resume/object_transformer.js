67b63ecb30574ea700af51575a2ec65ce19c0a52
aashish2604 <aashish.260401@gmail.com>
2023-07-08 15:54:42 +0530

    added error handling in object transformer

--- src/services/generate_resume/object_transformer.js
+++ src/services/generate_resume/object_transformer.js
@@ -1,61 +1,63 @@
-class ObjectTransformer{
-
-  #educationTransformer(data){
-    let responseData=[];
-    data.forEach(element => {
+class ObjectTransformer {
+  
+  #educationTransformer(data) {
+    let responseData = [];
+    data.forEach((element) => {
       responseData.push({
         SchoolName: element.school_name,
         Year: element.passing_year,
-        Description: element.description
-      })
+        Description: element.description,
+      });
     });
     return responseData;
   }
 
-  #experienceTransformer(data){
-    let responseData=[];
-    data.forEach(element => {
+  #experienceTransformer(data) {
+    let responseData = [];
+    data.forEach((element) => {
       responseData.push({
         CompanyName: element.company_name,
         Year: element.passing_year,
-        Description: element.responsibilities
-      })
+        Description: element.responsibilities,
+      });
     });
     return responseData;
   }
 
-  #achievementsTransformer(data){
-    let responseData=[];
-    data.forEach(element => {
+  #achievementsTransformer(data) {
+    let responseData = [];
+    data.forEach((element) => {
       responseData.push({
         Type: element.field,
-        Description: element.awards
-      })
+        Description: element.awards,
+      });
     });
     return responseData;
   }
 
-  transform(input){
-    if(Object.keys(input).length===0) return {};
-    let response;
-    response={
-      Name: input.personal_information.name,
-      LastName: input.personal_information.last_name,
-      EmailAddress: input.personal_information.email_address,
-      PhoneNumber: input.personal_information.phone_number,
-      LinkedIn: `<a href="${input.personal_information.linkedin_url}">linkedIn</a>`,
-      JobTitle: input.job_title,
-      Summary: input.career_objective,
-      Skills: input.skills,
-      Education: this.#educationTransformer(input.education),
-      Experience: this.#experienceTransformer(input.experience),
-      Achievements: this.#achievementsTransformer(input.achievements)
+  transform(input) {
+    try {
+      if (Object.keys(input).length === 0) return {};
+      let response;
+      response = {
+        Name: input.personal_information.name,
+        LastName: input.personal_information.last_name,
+        EmailAddress: input.personal_information.email_address,
+        PhoneNumber: input.personal_information.phone_number,
+        LinkedIn: `<a href="${input.personal_information.linkedin_url}">linkedIn</a>`,
+        JobTitle: input.job_title,
+        Summary: input.career_objective,
+        Skills: input.skills,
+        Education: this.#educationTransformer(input.education),
+        Experience: this.#experienceTransformer(input.experience),
+        Achievements: this.#achievementsTransformer(input.achievements),
+      };
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-    return response;
   }
-
-  
 }
 
-module.exports=ObjectTransformer;
-
+module.exports = ObjectTransformer;
