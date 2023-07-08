d2aa096ac56c486fa81a482958527b0d0c3ab333
aashish2604 <aashish.260401@gmail.com>
2023-07-08 16:35:11 +0530

    updated error middleware

--- src/middleware/error_middleware.js
+++ src/middleware/error_middleware.js
@@ -10,6 +10,21 @@ class ErrorHandlingMiddleware {
       resp.status(StatusConstants.code404).send(StatusConstants.code404Message);
     });
   }
+  async handle400Error() {
+    this.app.use((req, resp) => {
+      resp.status(StatusConstants.code400).send(StatusConstants.code400Message);
+    });
+  }
+  async handle401Error() {
+    this.app.use((req, resp) => {
+      resp.status(StatusConstants.code401).send(StatusConstants.code401Message);
+    });
+  }
+  async handle500Error() {
+    this.app.use((req, resp) => {
+      resp.status(StatusConstants.code500).send(StatusConstants.code500Message);
+    });
+  }
 }
 
 module.exports = ErrorHandlingMiddleware;
