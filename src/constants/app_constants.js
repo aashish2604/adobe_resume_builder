4618ad373a23980f2e0865b6390e1d9422c6386e
aashish2604 <aashish.260401@gmail.com>
2023-07-09 21:22:17 +0530

    feat: added unit testing for url parser

--- src/constants/app_constants.js
+++ src/constants/app_constants.js
@@ -1,6 +1,7 @@
 class AppConstants{
     ANCHOR_TAG_REGEX=/https?:\/\/[^\s\[\]]+\[([^\[\]]+)\]/g;
     PHONE_NUMBER_REGEX=/^[+]\d{1,3}\s?\d{3}\s?\d{3}\s?\d{2}\s?\d{2}$/;
+    URL_EXCL_ANCHOR_REGEX=/((https?|ftps?):\/\/[^"<\s]+)(?![^<>]*>|[^"]*?<\/a)/g;
     LINKEDIN_PROFILE_REGEX=/^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)\/([-a-zA-Z0-9]+)\/*$/;
     RESUME_LINKED_TEMPLATE="LinkTemplate.docx";
     RESUME_IMAGE_TEMPLATE="ImageTemplate.docx";
--- tests/business_layer/object_transformer.test.js
+++ tests/business_layer/object_transformer.test.js
@@ -35,4 +35,52 @@ describe('object transformer tester',()=>{
     test('Throws error is the input is invalid',()=>{
         expect(()=>objectTransformer.transform()).toThrow();
     });
-})
+});
+// Replace with the correct file path
+
+describe('urlParser', () => {
+  it('should replace URLs with anchor tags including inner text', () => {
+    const data = 'Check out this website: https://example.com[Example Website].';
+    const expected = 'Check out this website: <a href="https://example.com">Example Website</a>.';
+
+    const result = objectTransformer.urlParser(data);
+
+    expect(result).toBe(expected);
+  });
+
+  it('should replace multiple URLs with anchor tags including inner text', () => {
+    const data = 'Visit https://google.com[Google] and https://example.com[Example].';
+    const expected = 'Visit <a href="https://google.com">Google</a> and <a href="https://example.com">Example</a>.';
+
+    const result = objectTransformer.urlParser(data);
+
+    expect(result).toBe(expected);
+  });
+
+  it('should handle URLs without inner text', () => {
+    const data = 'Check this out: https://example.com';
+    const expected = 'Check this out: <a href="https://example.com">https://example.com</a>';
+
+    const result = objectTransformer.urlParser(data);
+
+    expect(result).toBe(expected);
+  });
+
+  it('should handle urls with inner text and without inner text in the same string',()=>{
+    const data = `There are many variations of passages of https://www.google.com[Lorem Ipsum] available, https://www.google.com but the https://tiktok.com[tik] and https://tiktok.com majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. Another example: https://www.adobe.co.in[Something else].`;
+    const expected = `There are many variations of passages of <a href="https://www.google.com">Lorem Ipsum</a> available, <a href="https://www.google.com">https://www.google.com</a> but the <a href="https://tiktok.com">tik</a> and <a href="https://tiktok.com">https://tiktok.com</a> majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. Another example: <a href="https://www.adobe.co.in">Something else</a>.`
+
+    const result = objectTransformer.urlParser(data);
+
+    expect(result).toBe(expected);
+  })
+
+  it('should not modify the text if there are no URLs', () => {
+    const data = 'This is a plain text without any URLs.';
+    const expected = 'This is a plain text without any URLs.';
+
+    const result = objectTransformer.urlParser(data);
+
+    expect(result).toBe(expected);
+  });
+});
